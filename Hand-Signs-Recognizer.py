5a4c010d5acd53f0d27a20ff770e62e4949668a5
Akash <akash.darknight@gmail.com>
2018-03-16 10:03:00 +0530

    Script

--- Hand-Signs-Recognizer.py
+++ Hand-Signs-Recognizer.py
@@ -131,3 +131,26 @@ def forward_propagation(X, parameters):
     # Retrieve the parameters from the dictionary "parameters"
     W1 = parameters['W1']
     W2 = parameters['W2']
+
+    ### START CODE HERE ###
+    # CONV2D: stride of 1, padding 'SAME'
+    Z1 = tf.nn.conv2d(X, W1, strides=[1, 1, 1, 1], padding='SAME')
+    # RELU
+    A1 = tf.nn.relu(Z1)
+    # MAXPOOL: window 8x8, sride 8, padding 'SAME'
+    P1 = tf.nn.max_pool(A1, ksize=[1, 8, 8, 1], strides=[1, 8, 8, 1], padding='SAME')
+    # CONV2D: filters W2, stride 1, padding 'SAME'
+    Z2 = tf.nn.conv2d(P1, W2, strides=[1, 1, 1, 1], padding='SAME')
+    # RELU
+    A2 = tf.nn.relu(Z2)
+    # MAXPOOL: window 4x4, stride 4, padding 'SAME'
+    P2 = tf.nn.max_pool(A2, ksize=[1, 4, 4, 1], strides=[1, 4, 4, 1], padding='SAME')
+    # FLATTEN
+    P2 = tf.contrib.layers.flatten(P2)
+    # FULLY-CONNECTED without non-linear activation function (not not call softmax).
+    # 6 neurons in output layer. Hint: one of the arguments should be "activation_fn=None"
+    Z3 = tf.contrib.layers.fully_connected(P2, 6, activation_fn=None)
+    ### END CODE HERE ###
+
+    return Z3
+
