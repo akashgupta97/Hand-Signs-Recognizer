f7415e9b7897451e7d270dbcc062fdb42d958698
Akash <akash.darknight@gmail.com>
2018-03-22 10:03:00 +0530

    Script

--- Hand-Signs-Recognizer.py
+++ Hand-Signs-Recognizer.py
@@ -184,3 +184,18 @@ def compute_cost(Z3, Y):
 
     return cost
 
+# In[41]:
+
+tf.reset_default_graph()
+
+with tf.Session() as sess:
+    np.random.seed(1)
+    X, Y = create_placeholders(64, 64, 3, 6)
+    parameters = initialize_parameters()
+    Z3 = forward_propagation(X, parameters)
+    cost = compute_cost(Z3, Y)
+    init = tf.global_variables_initializer()
+    sess.run(init)
+    a = sess.run(cost, {X: np.random.randn(4, 64, 64, 3), Y: np.random.randn(4, 6)})
+    print("cost = " + str(a))
+
